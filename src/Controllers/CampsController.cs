df5169c74d67427da71a2976a5ac1fab6c26c10c
Shawn Zhang <shawn.zhang@razerzone.com>
2019-06-21 07:50:47 +0800

    Add automapper support front end return payload

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,4 +1,6 @@
-﻿using CoreCodeCamp.Data;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,23 +15,67 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
 
-        public CampsController(ICampRepository campRepository)
+        public CampsController(ICampRepository campRepository, IMapper mapper)
         {
             this._campRepository = campRepository;
+            this._mapper = mapper;
         }
+
         //return action from this method, action could be success or failed, here is status code coming from
+        //[HttpGet]
+        //public async Task<IActionResult> GetCamps() {
+        //    try
+        //    {
+        //        var results = await _campRepository.GetAllCampsAsync();
+
+        //        CampModel[] models = this._mapper.Map<CampModel[]>(results);
+
+        //        return Ok(models);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failed");
+        //    }
+        //}
+
+        //we code refactoring from IActionResult to ActionResult<T>, return templated version
         [HttpGet]
-        public async Task<IActionResult> GetCamps() {
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
             try
             {
-                var result = await _campRepository.GetAllCampsAsync();
-                return Ok(result);
+                var results = await _campRepository.GetAllCampsAsync();
+
+                return _mapper.Map<CampModel[]>(results);
             }
             catch (Exception)
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failed");
             }
+        }
+
+        //detail route explaination
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> GetCampByMoniker(string moniker) {
+            try
+            {
+                var result = await _campRepository.GetCampAsync(moniker);
+
+                if (result == null) {
+                    return NotFound();
+                }
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failed");
+            }
+
         }
     }
 }
