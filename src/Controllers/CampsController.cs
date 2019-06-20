98f688464d2875f28d9a5e4a33ade4042024e491
Shawn Zhang <shawn.zhang@razerzone.com>
2019-06-20 22:15:26 +0800

    Adding first controller

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,35 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            this._campRepository = campRepository;
+        }
+        //return action from this method, action could be success or failed, here is status code coming from
+        [HttpGet]
+        public async Task<IActionResult> GetCamps() {
+            try
+            {
+                var result = await _campRepository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failed");
+            }
+        }
+    }
+}
