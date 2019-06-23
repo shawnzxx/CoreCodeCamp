aba7b6fcc24d465d733fa3fb988d5671ade2ed70
Shawn Zhang <shawn.zhang@razerzone.com>
2019-06-23 18:23:04 +0800

    add functional api

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+
+        [HttpOptions("reloadConfig")]
+        public IActionResult ReloadConfig() {
+            try
+            {
+                var rootConfig = (IConfigurationRoot)_configuration;
+                rootConfig.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
